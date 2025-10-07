08a5a018ee5e001b3e0592dca19ecb0b79bc1594
Thomas Rory Gummerson <thomas.gummerson@piql.com>
2025-10-07 09:46:47 +0200

    Remove autotools files

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Copyright (c) 2020 Piql AS
-
-set -e
-srcdir="$(dirname $0)"
-cd "$srcdir"
-which autoreconf pkg-config >/dev/null || \
-  (echo "configuration failed, please install autoconf first" && exit 1)
-autoreconf --install --force --warnings=all
